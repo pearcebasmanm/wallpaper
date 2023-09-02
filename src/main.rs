064a68357382ca24c6378000ab0aabe654cd2a41
Max <pearcebasmanm@gmail.com>
2023-09-02 14:51:07 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+use std::{
+    fs::{self, OpenOptions},
+    io::Read,
+    path::{Path, PathBuf},
+    process::Command,
+    thread,
+    time::Duration,
+};
+
+use itertools::Itertools;
+use toml::Table;
+
+fn main() {
+    let project_directory = dirs::home_dir()
+        .unwrap()
+        .join(".var/app/com.max.Wallpapers/");
+    fs::create_dir_all(&project_directory).unwrap();
+
+    let config: Table = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(project_directory.join("config.toml"))
+        .ok()
+        .and_then(|mut file| {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).ok()?;
+            contents.parse().ok()
+        })
+        .unwrap_or_default();
+
+    let recursive = config
+        .get("recursive")
+        .is_some_and(|value| value.as_bool() == Some(true));
+
+    let interval: u64 = config
+        .get("interval-seconds")
+        .and_then(|interval| interval.as_integer()?.try_into().ok())
+        .unwrap_or(5);
+
+    let images = find_images(&project_directory, recursive);
+
+    for image in images.iter().cycle() {
+        change_wallpaper(image);
+        thread::sleep(Duration::from_secs(interval));
+    }
+}
+
+const IMAGE_EXTENSIONS: [&str; 4] = ["png", "jpg", "jpeg", "svg"];
+
+fn find_images(path: &Path, recursive: bool) -> Vec<PathBuf> {
+    path.read_dir()
+        .unwrap()
+        .flatten()
+        .flat_map(|entry| {
+            let file_type = entry.file_type().ok()?;
+            if recursive && file_type.is_dir() {
+                Some(find_images(&entry.path(), true))
+            } else if IMAGE_EXTENSIONS.contains(&entry.path().extension()?.to_str()?) {
+                Some(vec![entry.path()])
+            } else {
+                None
+            }
+        })
+        .concat()
+}
+
+fn change_wallpaper(image: &Path) {
+    for theme in ["picture-uri", "picture-uri-dark"] {
+        Command::new("bash")
+            .arg("-c")
+            .arg(&format!(
+                "gsettings set org.gnome.desktop.background {theme} file://{}",
+                image.display()
+            ))
+            .output()
+            .unwrap();
+    }
+}
